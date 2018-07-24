7152916b45c94c1b3f1892d3688f35053bad48da
olofs <svedvall@gmail.com>
2018-07-24 15:25:06 +0200

    Städat upp koden. Gjort det snyggare.

--- problem_20_34.py
+++ problem_20_34.py
@@ -1,20 +1,19 @@
 from numpy import array, cross, sqrt
 from numpy.linalg import solve
 
+Moment = [40, -40, -20]
+Moment = array(Moment)
+Inertia = [[2, -3, 0],
+           [-3, 8, 0],
+           [0, 0, 10]]
+Inertia = array(Inertia)
+alpha = solve(Inertia, Moment)
+print(f"Alpha is: {alpha}")
 
-M = array([40, -40, -20])
-I = array([ [2, -3, 0],
-            [-3, 8, 0],
-            [0, 0, 10]])
-alpha = solve(I, M)
-print(f"Alpha is {alpha}")
-
-m = 12.
-F = array([20, 0, 40])
-acc_cm = F / m
+mass = 12.
+Force = array([20, 0, 40])
+acc_cm = Force / mass
 r_P = array([-1, -1, 0])
 acc_P = acc_cm + cross(alpha, r_P)
-print("Acceleration of point P = -1i + -1j is")
-print(acc_P)
-print("Total acceleration is")
-print(sqrt(acc_P.dot(acc_P)))
+print(f"Acceleration of point P = -1i + -1j is: {acc_P}")
+print(f"Total acceleration is: {sqrt(acc_P.dot(acc_P))}")
