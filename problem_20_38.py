bb5a96130bfb6de8a47f69f7db2ed0e2c535c476
Olof <svedvall@gmail.com>
2018-07-26 17:55:50 +0200

    Gjort två till problem.

---
+++ problem_20_38.py
@@ -0,0 +1,30 @@
+from numpy import array, sqrt
+from numpy.linalg import solve
+
+m_disk = 2
+m_bar = 3
+radius_disk = 0.2
+lenght_bar = 0.6
+M = [-10, 10, 0]
+I_bar = array([[0, 0, 0,],
+               [0, 1, 0],
+               [0, 0, 1] ]) * 1./12 * m_bar * lenght_bar**2
+I_bar_displacement = array([[0, 0, 0],
+                            [0, 0.2**2, 0],
+                            [0, 0, 0.2**2]]) * m_bar
+I_disk = array([[1./4, 0, 0],
+                [0, 1./4, 0],
+                [0, 0, 1./2]]) * m_disk * radius_disk**2
+I_disk_displacement = array([[0, 0, 0],
+                             [0, 0.3**2, 0],
+                             [0, 0, 0.3**2]]) * m_disk
+I = I_bar + I_bar_displacement + I_disk + I_disk_displacement
+I = array([[I[0, 0], -I[0, 1], -I[0, 2]],
+           [-I[1, 0], I[1, 1], -I[1, 2]],
+           [-I[2, 0], -I[2, 1], I[2, 2]]])
+print(I)
+alpha = solve(I, M)
+print('Angular acceleration is')
+print(alpha)
+print("Magnitude of angular acceleration is")
+print(sqrt(alpha.dot(alpha)))
---
+++ problem_20_44.py
@@ -0,0 +1,43 @@
+from numpy import matrix, pi, sqrt, array
+
+def IBarA(mass, length):
+    coeff = 1./12 * mass * length**2
+    I = [[coeff, 0, 0], [0, 0, 0], [0, 0, coeff]]
+    return matrix(I)
+
+def IBarB(mass, length):
+    coeff = 1./12 * mass * length**2
+    I = [[0, 0, 0], [0, coeff, 0], [0, 0, coeff]]
+    return matrix(I)
+
+def parrallelAxisBar(mass, delta_x, delta_y, delta_z):
+    xx = mass * (delta_y**2 + delta_z**2)
+    yy = mass * (delta_x**2 + delta_z**2)
+    zz = mass * (delta_y**2 + delta_x**2)
+    xy = delta_x * delta_y * mass
+    yx = xy
+    yz = delta_y * delta_z * mass
+    zy = yz
+    zx = delta_z * delta_x * mass
+    xz = zx
+    P = [[xx, -xy, -xz], [-yx, yy, -yz], [-zx, -zy, zz]]
+    return matrix(P)
+
+I_A_cm = IBarA(5000, 20)
+I_B_cm = IBarB(5000, 20)
+I_A = I_A_cm + parrallelAxisBar(5000, -5, 5, 0)
+I_B = I_B_cm + parrallelAxisBar(5000, 5, -5, 0)
+I = I_A + I_B
+print("Inertia matrix I")
+print(I)
+
+omega = [pi / 600., 0, 0]
+stor_omega = [[0, -omega[2], omega[1]],
+              [omega[2], 0, -omega[0]],
+              [-omega[1], omega[0], 0]]
+omega = matrix(omega)
+stor_omega = matrix(stor_omega)
+Moment = stor_omega * I * omega.transpose()
+vector_Moment = array(Moment.getA1())
+abs_Moment = sqrt(vector_Moment.dot(vector_Moment))
+print("The magnitude of the couple exterted by the engines is: ", abs_Moment)
