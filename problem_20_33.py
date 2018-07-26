831daff2f9ea32fe70ed7954db7e8ad2072b848b
Olof <svedvall@gmail.com>
2018-07-26 17:55:39 +0200

    Snyggat till koden.

--- problem_20_33.py
+++ problem_20_33.py
@@ -1,7 +1,7 @@
 from numpy import array
 from numpy.linalg import solve
 
-I = [[1./2, 2./3, 0], [2./3, 8./3, 0], [0, 0, 167./999]]
+I = [[1./2, -2./3, 0], [-2./3, 8./3, 0], [0, 0, 167./999]]
 M = [0, 0, -1200]
 I = array(I)
 M = array(M)
