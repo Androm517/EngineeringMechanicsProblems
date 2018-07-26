c5b4ce198a77cbc3bedcec8d00800b9b421cd0fa
Olof <svedvall@gmail.com>
2018-07-26 10:49:52 +0200

    Solved problem ch 20 ex 33

---
+++ problem_20_33.py
@@ -0,0 +1,8 @@
+from numpy import array
+from numpy.linalg import solve
+
+I = [[1./2, 2./3, 0], [2./3, 8./3, 0], [0, 0, 167./999]]
+M = [0, 0, -1200]
+I = array(I)
+M = array(M)
+print('angular acceleration alpha is: ', solve(I, M))
